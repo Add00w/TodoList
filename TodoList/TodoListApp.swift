bf92c7738d7e6828e276112533ceaeb715ba25a8
Abdullahi Addow <jiincade@gmail.com>
2023-01-18 12:16:37 +0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Abdullah Addow on 1/18/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
